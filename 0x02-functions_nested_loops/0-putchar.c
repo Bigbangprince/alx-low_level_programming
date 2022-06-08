feeb76589d83a4f1db0f0694edcfd4b31477807f
Enwongo_obong <riskkidsrichkids@gmail.com>
2022-06-08 05:26:05 -0700

    print putc

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,20 @@
 #include "main.h"
 /**
- * main - Entry point
- * prints _putchar as a message
- *
- * main prints _putchar
- * Return: On success 1
- * On error, -1 is returned, and errno is set appropriately
- */
+* main - Entry point
+*
+* Return: nothing, return void
+*/
 int main(void)
 {
-	int str[] = {45, 112, 117, 116, 99, 104, 97, 114};
-	int count, sz;
-
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
-	{
-		_putchar(str[count]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
