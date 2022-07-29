d878c594c246ced279bbdf119b36bcba83299d89
bigbangprince <riskkidsrichkids@gmail.com>
2022-07-29 15:53:38 -0700

    read

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,47 +1,36 @@
 #include "main.h"
-#include <stdlib.h>
 /**
-  * read_textfile - reads a text file and prints it to the POSIX stdout
-  * @filename: file to read
-  * @letters: number of letters it should read and print
-  * Return: number of letters it could read and print or 0 if fails
+  * read_textfile - reads a text file and prints to the POSIX stdout.
+  * @filename: pointer to name of file.
+  * @letters:  number of letters it should read and print.
+  *
+  * Return: number of letters it could print or write or 0.
   */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buff;
-	int of;
-	size_t lRead, check;
+	int file, rd, wr;
+	char *buf;
 
-	if (!filename || !letters)
+	if (filename == NULL)
 		return (0);
 
-	buff = malloc(letters);
-	if (!buff)
+	file = open(filename, O_RDONLY);
+	if (file == -1)
 		return (0);
-	/* open file */
-	of = open(filename, O_RDONLY);
-	if (of == -1)
-	{
-		free(buff);
+	buf = malloc(sizeof(char) * letters + 1);
+	if (buf == NULL)
 		return (0);
-	}
-	/* read file up to "letters" amount of charachters */
-	lRead = read(of, buff, letters);
-	if (lRead < 1)
-	{
-		free(buff);
-		close(of);
-		return (0);
-	}
-	/* write content read */
-	check = write(STDOUT_FILENO, buff, lRead);
-
-	free(buff);
-	close(of);
-
-	if (!check || check != lRead)
+	rd = read(file, buf, letters);
+	if (rd == -1)
 		return (0);
 
-	return (lRead);
+	buf[letters] = '\0';
+
+	wr = write(STDOUT_FILENO, buf, rd);
+	if (wr == -1)
+		return (0);
+
+	close(file);
+	free(buf);
+	return (wr);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,16 +1,19 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <elf.h>
+/* library files*/
+
 #include <stdlib.h>
-#include <stdio.h>
 #include <unistd.h>
-#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+/*function prototypes*/
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif _MAIN_H_
+#endif /* MAIN_H */
